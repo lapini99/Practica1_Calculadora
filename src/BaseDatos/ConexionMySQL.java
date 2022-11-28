31b8bebad526c6ed208a821b8ec21607ddab383e
Alessandro Lapini <76733012+lapini99@users.noreply.github.com>
2022-11-28 08:42:44 +0100

    Conectar calculadora a db
    Realiza la conexión a db y testea si esta funciona

---
+++ src/BaseDatos/ConexionMySQL.java
@@ -0,0 +1,32 @@
+/*import java.sql.*;
+import com.mysql.jdbc.Connection;
+
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseDatos;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+    public class ConexionMySQL {
+    public String bd = "calculadoralapini";
+    public String login = "root";
+    public String password = "";
+    public String url = "jdbc:mysql://localhost/" + bd;
+
+    public Connection connect() {
+        Connection link = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            link = DriverManager.getConnection(this.url, this.login, this.password);
+           // JOptionPane.showMessageDialog(null ,"Conexión establecida");
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null , e);
+        }
+        return link;
+    }
+
+}
